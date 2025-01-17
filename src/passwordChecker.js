71f27f7aa02a50346ca9c2c6c17fb31e0d051709
Emre <emre.emin@student.vinci.be>
2025-01-17 17:08:15 +0100

    feat: add special character validation to passwordChecker

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -2,5 +2,8 @@ export class Main {
     checkMinCharacters(password) {
         return password.length >= 8;
     }
-    
+    checkSpecialCharacter(password) {
+        ;
+    return /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    }
 }
