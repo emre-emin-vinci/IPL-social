88dc66201163cd8119a3a91ab39b0e891828251c
Emre <emre.emin@student.vinci.be>
2025-01-17 17:21:22 +0100

    feat: add password validation methods for minimum length, special character, and number checks

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,3 +1,12 @@
 export class Main {
-    
+    checkMinCharacters(password) {
+        return password.length >= 8;
+    }
+    checkSpecialCharacter(password) {
+        ;
+    return /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    }
+    checkNumber(password) {
+        return /[0-9]/.test(password);
+    }
 }
