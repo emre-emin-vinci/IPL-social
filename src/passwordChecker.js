2a96416dd4296838e311d0afcc69e0b6572b57a5
Emre <emre.emin@student.vinci.be>
2025-01-17 17:27:00 +0100

    feat: implement password validation methods

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,3 +1,17 @@
 export class Main {
-    
+    checkMinCharacters(password) {
+        return password.length >= 8;
+    }
+    checkSpecialCharacter(password) {
+        ;
+    return /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    }
+    checkNumber(password) {
+        return /[0-9]/.test(password);
+    }
+    checkNotIncludePassword(password) {
+        return !password.includes("IPL");
+    }
+
 }
+
